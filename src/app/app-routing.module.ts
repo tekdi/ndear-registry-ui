a78e5d4cce5ec70eeacc8cc23a52648733a6d77a
raghavecode <raghavecode@gmail.com>
2021-05-03 10:24:00 +0530

    local changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { StudentProfileComponent } from './components/student-profile/student-profile.component';
+import { StudentTeacherLoginComponent } from './components/student-teacher-login/student-teacher-login.component';
 import { StudentTeacherSignupComponent } from './components/student-teacher-signup/student-teacher-signup.component';
+import { VerificationComponent } from './components/verification/verification.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'student-teacher-signup', component: StudentTeacherSignupComponent },
+  { path: 'student-teacher-login', component: StudentTeacherLoginComponent },
+  { path: 'verification', component: VerificationComponent },
+  { path: 'student-profile', component: StudentProfileComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,48 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { JsonFormsModule } from '@jsonforms/angular';
-import { JsonFormsAngularMaterialModule } from '@jsonforms/angular-material';
 import { AppComponent } from './app.component';
-import { CustomAutocompleteControlRenderer } from './custom.autocomplete';
-import { DataDisplayComponent } from './data.control';
-import { LangComponent } from './lang.control';
 import { HomeComponent } from './components/home/home.component';
 import { StudentTeacherSignupComponent } from './components/student-teacher-signup/student-teacher-signup.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MdbModule } from 'mdb-angular-ui-kit';
+import { HeaderComponent } from './components/header/header.component';
+import { MaterialModule } from './material.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VerificationComponent } from './components/verification/verification.component';
+import { NgOtpInputModule } from  'ng-otp-input';
+import { StudentProfileComponent } from './components/student-profile/student-profile.component';
+import { StudentTeacherLoginComponent } from './components/student-teacher-login/student-teacher-login.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomAutocompleteControlRenderer,
-    LangComponent,
-    DataDisplayComponent,
     HomeComponent,
-    StudentTeacherSignupComponent
+    StudentTeacherSignupComponent,
+    HeaderComponent,
+    VerificationComponent,
+    StudentProfileComponent,
+    StudentTeacherLoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MdbModule,
     BrowserAnimationsModule,
-    JsonFormsModule,
-    JsonFormsAngularMaterialModule,
-    MatAutocompleteModule,
-    MatProgressSpinnerModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgOtpInputModule
   ],
   schemas: [],
-  entryComponents: [CustomAutocompleteControlRenderer, LangComponent, DataDisplayComponent],
+  entryComponents: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {

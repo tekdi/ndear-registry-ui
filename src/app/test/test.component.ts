0667436771a4e8c075a8fd93ff9e8331431e5509
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2021-06-24 23:00:41 +0530

    test

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -12,8 +12,6 @@ import { SchemaService } from 'src/app/services/data/schema.service';
   styleUrls: ['./test.component.css']
 })
 
-
-
 export class TestComponent implements OnInit {
 
   editSchema;
